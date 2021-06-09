c53cc930a6dab6ee4e9e7cc8d2523721deab1665
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-09 13:49:42 +0100

    Initial Commit

---
+++ WordScrumble/WordScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrumbleApp.swift
+//  WordScrumble
+//
+//  Created by administrator on 09/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
